d59384edafafb7d95e8375dea052b15c4d8da9d8
romaintailhurat <romain.tailhurat@gmail.com>
2017-05-15 16:31:24 +0200

    Triangle service is now listening to events produced via the UI

--- triangle-service/README.md
+++ triangle-service/README.md
@@ -1,3 +1,15 @@
 # Triangle service
 
 A simple UI emitting triangle events on user action.
+
+## Run
+
+With pipenv:
+
+```bash
+FLASK_APP=app.py pipenv run flask run
+```
+
+With Docker:
+
+<soon>
--- triangle-service/app.py
+++ triangle-service/app.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
+from flask import Flask, render_template
 import pika
 import sys
+from datetime import datetime
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
+FLAGS = { "BROKER_ON": False }
 
-channel.exchange_declare(exchange='triangles',
-                         type='fanout')
+app = Flask(__name__)
 
-message = "shape: Triangle!"
-channel.basic_publish(exchange='triangles',
-                      routing_key='',
-                      body=message)
-print(" [x] Sent %r" % message)
-connection.close()
+@app.route("/")
+def home():
+    return render_template("index.html", timestamp=datetime.now())
+
+if FLAGS["BROKER_ON"]:
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange='triangles', type='fanout')
+    message = "shape: Triangle!"
+    channel.basic_publish(exchange='triangles', routing_key='', body=message)
+    print(" [x] Sent %r" % message)
+    connection.close()
