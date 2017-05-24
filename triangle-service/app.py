c47a3b52aa39187016f9281b96338f65946e9e41
romaintailhurat <romain.tailhurat@gmail.com>
2017-05-24 16:32:43 +0200

    Pushing events to the triangle service

--- triangle-service/app.py
+++ triangle-service/app.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import pika
 import sys
+import json
 from datetime import datetime
 
 FLAGS = { "BROKER_ON": False }
 
 app = Flask(__name__)
 
+# Simple stupid memory storage
+tempStore = []
+
 @app.route("/")
 def home():
     return render_template("index.html", timestamp=datetime.now())
+
+@app.route("/events", methods=["GET"])
+def getEvents():
+    return jsonify(tempStore)
+
+@app.route("/events", methods=["POST"])
+def pushEvents():
+    payload = request.data
+    tempStore.append(json.loads(payload))
+    return jsonify({"status":"message received"})
 
 if FLAGS["BROKER_ON"]:
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
