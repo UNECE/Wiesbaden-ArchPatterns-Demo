95444515a246a2c0e25f8dca6bd23d7f8295cea5
romaintailhurat <romain.tailhurat@gmail.com>
2017-06-22 12:26:04 +0200

    Changes in the structure of the message sent by the collect-service

--- process-service/src/index.js
+++ process-service/src/index.js
@@ -1,21 +1,31 @@
-import amqp from "amqplib/callback_api"
-import express from "express"
+import amqp from "amqplib/callback_api";
+import express from "express";
+import process from "./process";
 
 const port = 3000;
-const app = express()
+const app = express();
 
+/* Handling connection to the broker, both for sending and receiving messages */
 const connectToBroker = () => {
   amqp.connect("amqp://broker:5672", (err, conn) => {
     console.log("Connecting to broker");
     conn.createChannel(function(err, ch) {
-      var ex = 'collect-channel';
-      ch.assertExchange(ex, 'fanout', {durable: false});
-      ch.assertQueue('', {exclusive: true}, function(err, q) {
-        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
-        ch.bindQueue(q.queue, ex, '');
-        ch.consume(q.queue, function(msg) {
-          console.log(" [x] %s", msg.content.toString());
-        }, {noAck: true});
+      var ex = "collect-channel";
+      ch.assertExchange(ex, "fanout", { durable: false });
+      ch.assertQueue("", { exclusive: true }, function(err, q) {
+        console.log(
+          " [*] Waiting for messages in %s.",
+          q.queue
+        );
+        ch.bindQueue(q.queue, ex, "");
+        ch.consume(
+          q.queue,
+          function(msg) {
+            console.log(" [x] Message to process: %s", msg.content.toString());
+            //process(msg.content);
+          },
+          { noAck: true }
+        );
       });
     });
   });
@@ -31,5 +41,7 @@ app.get("/activate", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Service running on ${port}`);
-  setTimeout(() => { connectToBroker() }, 10000);
+  setTimeout(() => {
+    connectToBroker();
+  }, 10000);
 });
